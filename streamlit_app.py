0fd88eba76eebc6c243afac0c9d095442846e32a
Omari Kwache <justusomari@gmail.com>
2026-02-20 13:11:52 +0300

    Checking automation

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,8 @@ with st.form(key="fraud_predictions"):
         result=(response.json())
         st.write(f"### Evaluation Result: {result["is_fraud"]}")
         st.write(f"### Probability: {result["fraud_probability"]*100}%")
-st.write("Threshold in use",threshold)
+st.write("Threshold in use")
+
         
 
         
